487e0c6ff93c89206b1190a38826626938656412
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2022-12-16 18:15:07 +0100

    Add JWT Auth

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,14 @@
 const asyncHandler = require("express-async-handler");
+const { restart } = require("nodemon");
 
 const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
 
 // @desc    Get Goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
+  const goals = await Goal.find({ user: req.user.id });
 
   res.status(200).json(goals);
 });
@@ -22,6 +24,7 @@ const setGoal = asyncHandler(async (req, res) => {
 
   const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   });
 
   res.status(200).json(goal);
@@ -36,6 +39,20 @@ const updateGoal = asyncHandler(async (req, res) => {
   if (!goal) {
     res.status(400);
     throw new Error("Goal not found");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  // Check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found.");
+  }
+
+  // Check if logged in user matches goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized.");
   }
 
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
@@ -54,6 +71,20 @@ const deleteGoal = asyncHandler(async (req, res) => {
   if (!goalToDelete) {
     res.status(400);
     throw new Error("Goal not found");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  // Check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found.");
+  }
+
+  // Check matching between logged in and goal user
+  if (goalToDelete.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized.");
   }
 
   await goalToDelete.remove();
